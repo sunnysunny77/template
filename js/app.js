f95153c8c778a166b47a3f5b97beeecf9956e9c4
daniel <contractor@lateralaspect.com.au>
2024-06-21 14:51:51 +0800

    up

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-function callback () {
-    
+const events = (obj,typ,callback,opts) => {
+  if (obj) {
+    obj.addEventListener(typ,callback,opts);
+  }
+};
 
-}
+const callback = () => {
+};
 
-function events (obj,typ,callback,opts) {
-    if (obj) {
-        obj.addEventListener(typ,callback,opts);
-    }
-}
 
-window.onload = function () {
-//events(document.getElementById('tmp'),"click",callback,null);
-}
+events(window, "load",  () => {
+  callback();
+});
