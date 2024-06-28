106d6815c8a5d5b3a079d5a3eb701d76496e6c5d
daniel <contractor@lateralaspect.com.au>
2024-06-29 00:50:43 +0800

    up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-import "../node_modules/bootstrap/js/src/collapse.js";
-import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
+
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
 import { events } from "./utillites.js";
 
 events(window, "load", () => {
