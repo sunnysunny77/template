73086558f27c450b67991a6c36c9e016aef452d1
daniel <contractor@lateralaspect.com.au>
2024-06-29 09:32:30 +0800

    up

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 
-
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import '../node_modules/bootstrap/js/dist/alert';
+// import '../node_modules/bootstrap/js/dist/button';
+// import '../node_modules/bootstrap/js/dist/carousel';
+import '../node_modules/bootstrap/js/dist/collapse';
+// import '../node_modules/bootstrap/js/dist/dropdown';
+// import '../node_modules/ootstrap/js/dist/modal';
+// import '../node_modules/bootstrap/js/dist/offcanvas';
+// import '../node_modules/bootstrap/js/dist/popover';
+// import '../node_modules/bootstrap/js/dist/scrollspy';
+// import '../node_modules/bootstrap/js/dist/tab';
+// import '../node_modules/bootstrap/js/dist/toast';
+// import '../node_modules/bootstrap/js/dist/tooltip';
 import "../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
 import { events } from "./utillites.js";
 
