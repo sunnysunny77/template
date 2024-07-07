6e8aba95ccfdc7c05bf596e8db40f118bb043107
daniel <contractor@lateralaspect.com.au>
2024-07-07 15:30:56 +0800

    Update worker.js

--- site/worker.js
+++ site/worker.js
@@ -1,36 +1,62 @@
-self.addEventListener("install", (event) => {
-  
+const version = 'v1';
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(version);
+  await cache.addAll(resources);
+};
+
+self.addEventListener('install', (event) => {
+  console.log(`${version} installing...`);
   event.waitUntil(
-    caches
-      .open("v1")
-      .then((cache) =>
-        cache.addAll([
-          "./",
-          "./index.php",
-          "./css/app.min.css",
-          "./js/app.min.js",
-          "/font/Poppins-Regular.ttf"
-        ])
-      )
+    addResourcesToCache([
+      "./",
+      "./index.php",
+      "./css/app.min.css",
+      "./js/app.min.js",
+      "./font/Poppins-Regular.ttf"
+    ])
   );
 });
 
-self.addEventListener("fetch", (event) => {
+async function fetchAndCacheIfOk(event) {
+  try {
+    const response = await fetch(event.request);
 
-  if (event.request.method !== "GET") return;
+    // don't cache non-ok responses
+    if (response.ok) {
+      const responseClone = response.clone();
+      const cache = await caches.open(version);
+      await cache.put(event.request, responseClone);
+    }
 
-  event.respondWith(
-    (async () => {
+    return response;
+  } catch (e) {
+    return e;
+  }
+}
 
-      const cache = await caches.open("v1");
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
+async function fetchWithCache(event) {
+  const cache = await caches.open(version);
+  const response = await cache.match(event.request);
+  if (!!response) {
+    // it is cached but we want to update it so request but not await
+    fetchAndCacheIfOk(event);
+    // return the cached response
+    return response;
+  } else {
+    // it was not cached yet so request and cache
+    return fetchAndCacheIfOk(event);
+  }
+}
 
-        if (navigator.onLine) event.waitUntil(cache.add(event.request));
-        return cachedResponse;
-      }
+function handleFetch(event) {
+  // only intercept the request if there is no no-cache header
+  if (event.request.headers.get("cache-control") !== "no-cache") {
+    // important: respondWith has to be called sync, otherwise
+    // the service worker won't know whats going on.
+    // Had to learn this the hard way
+    event.respondWith(fetchWithCache(event));
+  }
+}
 
-      return fetch(event.request);
-    })()
-  );
-});
+self.addEventListener("fetch", handleFetch);
