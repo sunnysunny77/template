ddecb5965f98d3d06c536742e52e91d7ea751b63
daniel <contractor@lateralaspect.com.au>
2024-07-10 17:29:38 +0800

    up

--- mkcert.sh
+++ mkcert.sh
@@ -4,7 +4,7 @@ if [ -e "server.crt" ] && [ -e "server.key" ] ; then
     echo "sll exist's"
 else
 openssl genrsa -des3 -passout pass:developemnt -out ca.key 2048 &&
-openssl req -x509 -new -nodes -key ca.key -passin pass:developemnt -sha256 -days 1024 -out ca.crt  -subj '/CN=dev.localhost Root CA/C=AU/ST=Western Australia/L=Perth/O=Web Developers/OU=Dev' &&
+openssl req -x509 -new -nodes -key ca.key -passin pass:developemnt -sha256 -days 1024 -out ca.crt  -subj \\CN=dev.localhost Root CA\\C=AU\\ST=Western Australia\\L=Perth\\O=Web Developers\\OU=Dev &&
 openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config csr.conf &&
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -passin pass:developemnt -CAcreateserial -out server.crt -days 500 -sha256 -extfile cert.conf;
 npm run install-cert
