9306c0accefc270f8273f9e403f0ace25503c005
daniel <contractor@lateralaspect.com.au>
2024-07-12 10:44:47 +0800

    up

--- mkcert.sh
+++ mkcert.sh
@@ -3,45 +3,54 @@
 source $INIT_CWD/.env
 
 echo -e "[ req ]\n\
-default_bits = 2048\n\
 prompt = no\n\
-default_md = sha256\n\
+distinguished_name  = dn\n\
+[ dn ]\n\
+C = AU\n\
+ST = Western Australia\n\
+L = Perth\n\
+O = Web Developers\n\
+OU = Dev\n\
+CN = $FQDN Root Ca" > ca.conf 
+
+echo -e "[ req ]\n\
+prompt = no\n\
 req_extensions = req_ext\n\
 distinguished_name = dn\n\
 [ dn ]\n\
 C = AU\n\
-ST = Perth\n\
-L = Western Australia\n\
+ST = Western Australia\n\
+L = Perth\n\
 O = Web Developers\n\
 OU = Dev\n\
 CN = $FQDN\n\
 [ req_ext ]\n\
 subjectAltName = @alt_names\n\
 [ alt_names ]\n\
-DNS.1 = $FQDN" > csr.conf
+DNS.1 = $FQDN" > csr.conf 
 
 echo -e "authorityKeyIdentifier=keyid,issuer\n\
 basicConstraints=CA:FALSE\n\
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment\n\
 subjectAltName = @alt_names\n\
 [alt_names]\n\
-DNS.1 = $FQDN" > cert.conf
+DNS.1 = $FQDN" > cert.conf 
 
 openssl genrsa \
 -des3 \
 -passout pass:developemnt \
--out ca.key 2048 &&
+-out ca.key 2048
 
-echo | openssl req \
+openssl req \
 -x509 \
 -new \
 -sha256 \
 -days 1024 \
 -out ca.crt \
--nodes \
+-nodes  \
 -key ca.key \
--passin pass:developemnt \
--subj "//CN=$FQDN Root CA/C=AU/ST=Western Australia/L=Perth/O=Web Developers/OU=Dev"
+-passin pass:developemnt  \
+-config ca.conf 
 
 openssl req \
 -new \
@@ -50,7 +59,7 @@ openssl req \
 -out server.csr \
 -newkey rsa:2048 \
 -keyout server.key \
--config csr.conf
+-config csr.conf 
 
 openssl x509 \
 -req \
@@ -62,6 +71,6 @@ openssl x509 \
 -out server.crt \
 -days 500 \
 -sha256 \
--extfile cert.conf; 
+-extfile cert.conf
 
 npm run install-cert
