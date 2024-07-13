4acf5d43b1e9c8da1cc6d844caed8093d67f8e1f
daniel <contractor@lateralaspect.com.au>
2024-07-14 00:13:43 +0800

    up

--- open.js
+++ open.js
@@ -1,4 +1,4 @@
 import open from "open";
 
-await open(`https://${process.env.FQDN}:3000`);
-console.log(`running on https://${process.env.FQDN}:3000`);
+await open(`https://${process.env.CN}:3000`);
+console.log(`running on https://${process.env.CN}:3000`);
