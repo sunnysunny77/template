e61fa035f31567e2b272922c08cb1a1791ce9f0a
daniel <contractor@lateralaspect.com.au>
2024-07-30 15:33:51 +0800

    up

--- serve.cjs
+++ serve.cjs
@@ -1,10 +1,12 @@
 const https = require("https");
 const http = require("http");
 const fs = require("fs");
-const express = require("express");
-const app = express();
-app.use(express.static("site"));
-app.get("/", function(req, res) {
+
+https.createServer({
+    key: fs.readFileSync("./certs/server.key"),
+    cert: fs.readFileSync("./certs/server.crt"),
+    passphrase: process.env.PASSWORD
+},(req, res) => {
     req.pipe(http.request({
         host: "127.0.0.1",
         port: "2998",
@@ -16,8 +18,4 @@ app.get("/", function(req, res) {
         res.writeHead(resp.statusCode,resp.headers);
         resp.pipe(res);
     }));
-});
-https.createServer({
-    key: fs.readFileSync("./certs/server.key"),
-    cert: fs.readFileSync("./certs/server.crt")
-},app).listen(3000);
+}).listen(3000, process.env.CN);
