b81b2e664f6cd50df82efef34b552af176df295a
daniel <contractor@lateralaspect.com.au>
2024-08-31 08:29:45 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
 //import "../node_modules/bootstrap/js/dist/tab";
 ///import "../node_modules/bootstrap/js/dist/toast";
 //import "../node_modules/bootstrap/js/dist/tooltip";
-//import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
+//import "../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
 //import { service_worker } from "./service_worker.js";
 import { events } from "./utillites.js";
 
