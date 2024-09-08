a583a7446a3db574aa7e2532877f5215f741b692
daniel <contractor@lateralaspect.com.au>
2024-09-08 18:05:39 +0800

    up

--- js/index.js
+++ js/index.js
@@ -10,11 +10,13 @@
 //import "../node_modules/bootstrap/js/dist/tab";
 ///import "../node_modules/bootstrap/js/dist/toast";
 //import "../node_modules/bootstrap/js/dist/tooltip";
+//import  AOS from "../node_modules/aos/dist/aos.js";
 //import "../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
 //import { service_worker } from "./service_worker.js";
 import { events } from "./utillites.js";
 
 events(window, "load", () => {
 
+    //AOS.init({once: true}); 
     //service_worker();
 });
