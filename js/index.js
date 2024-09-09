ca1081d5c34186d0d3f158b5c6f8f661cbadf3a2
daniel <contractor@lateralaspect.com.au>
2024-09-09 13:54:04 +0800

    up

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //import "../node_modules/bootstrap/js/dist/alert";
 //import "../node_modules/bootstrap/js/dist/button";
 //import "../node_modules/bootstrap/js/dist/carousel";
-//import "../node_modules/bootstrap/js/dist/collapse";
+import "../node_modules/bootstrap/js/dist/collapse";
 //import "../node_modules/bootstrap/js/dist/dropdown";
 //import "../node_modules/ootstrap/js/dist/modal";
 //import "../node_modules/bootstrap/js/dist/offcanvas";
@@ -10,13 +10,12 @@
 //import "../node_modules/bootstrap/js/dist/tab";
 ///import "../node_modules/bootstrap/js/dist/toast";
 //import "../node_modules/bootstrap/js/dist/tooltip";
-//import  AOS from "../node_modules/aos/dist/aos.js";
-//import "../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
-//import { service_worker } from "./service_worker.js";
+import  AOS from "../node_modules/aos/dist/aos.js";
+import { service_worker } from "./service_worker.js";
 import { events } from "./utillites.js";
 
 events(window, "load", () => {
 
-    //AOS.init({once: true}); 
-    //service_worker();
+    AOS.init({once: true}); 
+    service_worker();
 });
--- site/index.php
+++ site/index.php
@@ -1,24 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8" />
-  <!--
   <script src="./js/preload.js"></script>
-  <link rel="preload" href="./font/Poppins-Black.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-Bold.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-ExtraBold.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-ExtraLight.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-Light.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-Medium.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-Regular.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-SemiBold.ttf" as="font" type="font/ttf" crossorigin="" />
-  <link rel="preload" href="./font/Poppins-Thin.ttf" as="font" type="font/ttf" crossorigin="" />
-  -->
+  <meta charset="utf-8" />
   <meta name="description" content="" />
   <meta name="keywords" content="" />
   <meta name="author" content="" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title></title>
+  <title>HELLO WORLD</title>
   <link href="./css/app.min.css" rel="stylesheet" type="text/css" />
   <link rel="manifest" href="manifest.json" />
   <link rel="apple-touch-icon" href="images/pwa-logo-small.webp" />
@@ -32,9 +21,9 @@
 
         <div class="row justify-content-center">
 
-          <div class="col-11 col-sm-24 text-center mt-11">
+          <div data-aos="fade-right" class="col-11 col-sm-24 text-center mt-11">
 
-            HELLO WORLD
+            HELLO WORLD <i class="fa-regular fa-heart"></i>
             
           <div>
 
