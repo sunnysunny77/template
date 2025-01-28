94f70dd405059ee2cc9a2fbe693d255035f82385
daniel <contractor@lateralaspect.com.au>
2025-01-28 17:49:02 +0800

    up

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 //import "../node_modules/bootstrap/js/dist/alert";
 //import "../node_modules/bootstrap/js/dist/button";
 //import "../node_modules/bootstrap/js/dist/carousel";
-import "../node_modules/bootstrap/js/dist/collapse";
+//import "../node_modules/bootstrap/js/dist/collapse";
 //import "../node_modules/bootstrap/js/dist/dropdown";
 //import "../node_modules/ootstrap/js/dist/modal";
 //import "../node_modules/bootstrap/js/dist/offcanvas";
 //import "../node_modules/bootstrap/js/dist/popover";
 //import "../node_modules/bootstrap/js/dist/scrollspy";
 //import "../node_modules/bootstrap/js/dist/tab";
-///import "../node_modules/bootstrap/js/dist/toast";
+//import "../node_modules/bootstrap/js/dist/toast";
 //import "../node_modules/bootstrap/js/dist/tooltip";
-import  AOS from "../node_modules/aos/dist/aos.js";
+
+import  AOS from "aos";
 import { service_worker } from "./service_worker.js";
 import { events } from "./utillites.js";
 import { fallback } from "./fallback.js";
